f57d900bcfe6f97885675296d3413e2db04ee609
Ilja Busch <ilja.ragnar@gmail.com>
2021-12-05 20:10:17 +0100

    added auth requirement

--- src/services/approve-tuv/approve-tuv.hooks.ts
+++ src/services/approve-tuv/approve-tuv.hooks.ts
@@ -1,8 +1,12 @@
 import { HooksObject } from '@feathersjs/feathers';
+import * as authentication from '@feathersjs/authentication';
+// Don't remove this comment. It's needed to format import lines nicely.
+
+const { authenticate } = authentication.hooks;
 
 export default {
   before: {
-    all: [],
+    all: [ authenticate('jwt') ],
     find: [],
     get: [],
     create: [],
